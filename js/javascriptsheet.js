72f48016647aab2490aeaca4e0446c9c8e6121c7
Maxarsene <bruno.drozdz@live.fr>
2021-11-04 14:34:40 +0100

    tpprompt

--- js/javascriptsheet.js
+++ js/javascriptsheet.js
@@ -1,7 +1,14 @@
-let prenom = "Bruno";
+/*let prenom = "Bruno";
 let nom = "DROZDZ";
 let age = 48 ;
-let ville = "Bezons";
+let ville = "Bezons";*/
 /*alert("Je m'appelle " + prenom + " " + nom + ", je réside à " + ville + "\net je suis âgé de " + age + " ans." );*/
+
+/*alert(`Je m'appelle ${prenom} ${nom}, je réside à ${ville} \net je suis âgé de ${age} ans.` );*/
+
+let prenom = prompt("Saisissez votre prénom : ");
+let nom = prompt("Saisissez votre nom : ");
+let age = prompt("Saisissez votre âge : ");
+let ville = prompt("Saisissez votre ville de résidence : ");
 
 alert(`Je m'appelle ${prenom} ${nom}, je réside à ${ville} \net je suis âgé de ${age} ans.` );
