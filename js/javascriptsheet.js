e86b1281f8c4197d86ee54a6d857dcdfd7a581bf
Maxarsene <bruno.drozdz@live.fr>
2021-11-04 12:47:44 +0100

    tpnom

--- js/javascriptsheet.js
+++ js/javascriptsheet.js
@@ -1,1 +1,7 @@
-alert("Coucou");
+let prenom = "Bruno";
+let nom = "DROZDZ";
+let age = 48 ;
+let ville = "Bezons";
+/*alert("Je m'appelle " + prenom + " " + nom + ", je réside à " + ville + "\net je suis âgé de " + age + " ans." );*/
+
+alert(`Je m'appelle ${prenom} ${nom}, je réside à ${ville} \net je suis âgé de ${age} ans.` );
