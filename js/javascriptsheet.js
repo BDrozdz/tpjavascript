f989caff77c7bea342cac2b250867eb02d9b7d6f
Maxarsene <bruno.drozdz@live.fr>
2021-11-05 09:14:01 +0100

    string

--- js/javascriptsheet.js
+++ js/javascriptsheet.js
@@ -6,9 +6,17 @@ let ville = "Bezons";*/
 
 /*alert(`Je m'appelle ${prenom} ${nom}, je réside à ${ville} \net je suis âgé de ${age} ans.` );*/
 
+/*
 let prenom = prompt("Saisissez votre prénom : ");
 let nom = prompt("Saisissez votre nom : ");
 let age = prompt("Saisissez votre âge : ");
 let ville = prompt("Saisissez votre ville de résidence : ");
 
 alert(`Je m'appelle ${prenom} ${nom}, je réside à ${ville} \net je suis âgé de ${age} ans.` );
+*/
+
+/*
+console.log("Test", 3, true);
+
+Math.floor(Math.random() * 100)
+*/
---
+++ js/string.js
@@ -0,0 +1,16 @@
+/* let fisrtname = "Bruno";
+let lastname = "DROZDZ";*/
+
+let fisrtname = prompt("Saisissez votre prénom : ");
+let lastname = prompt("Saisissez votre nom : ");
+
+/*fisrtname = fisrtname.toUpperCase;
+lastname = lastname.toUpperCase;*/
+
+let test = fisrtname[0] +  lastname[0];
+
+test = test.toUpperCase();
+
+/*est= test.trim(test);*/
+
+alert(test);
